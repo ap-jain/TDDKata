fd4b8297f69c3fa44e4a512fea35a991b80acf22
Arpit Jain <arpitjain22june@gmail.com>
2021-09-09 14:44:24 +0530

    method9

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -21,7 +21,10 @@ public class StringCalculator {
 			nos = numbers.split(",|\\\\n");	
 			}
 			int s = 0;
-			for(int i=0;i<nos.length;i++) {				
+			for(int i=0;i<nos.length;i++) {	
+				if(Integer.parseInt(nos[i])>1000) {
+					continue;
+				}
 				s+=Integer.parseInt(nos[i]);
 				if(Integer.parseInt(nos[i])<0) {
 					throw new Exception ("negatives not allowed"+numbers);
