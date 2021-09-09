cebcb0bfaa869aa96833f3a2e5cebdeac0e4dad3
Arpit Jain <arpitjain22june@gmail.com>
2021-09-09 14:37:02 +0530

    method4

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,14 +2,25 @@ import java.util.Scanner;
 
 public class StringCalculator {
 	public static int Add(String numbers) {
+		char x = ' ';
+		boolean skip = false;
 		if(numbers.length()==0) {
 			return 0;
-		}else if(numbers.contains(",")||numbers.contains("\\n")) {
-			String[] nos = numbers.split(",|\\\\n");			
+		}
+		if(numbers.startsWith("//")){
+			x = numbers.charAt(2);			
+			skip = true;
+		}
+		if(numbers.contains(",")||numbers.contains("\\n")) {
+			String[] nos;
+			if(skip) {
+				nos =  numbers.substring(5).split(x+"|,|\\\\n");				
+			}else {
+			nos = numbers.split(",|\\\\n");	
+			}
 			int s = 0;
-			for(int i=0;i<nos.length;i++) {
-				s+=Integer.parseInt(nos[i]);
-				
+			for(int i=0;i<nos.length;i++) {				
+				s+=Integer.parseInt(nos[i]);				
 			}
 			return s;
 		}
