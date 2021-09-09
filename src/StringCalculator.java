fc305fce9a8478f456f1dd09198d7d8d1a0fcf28
Arpit Jain <arpitjain22june@gmail.com>
2021-09-09 14:23:07 +0530

    method3

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -4,8 +4,8 @@ public class StringCalculator {
 	public static int Add(String numbers) {
 		if(numbers.length()==0) {
 			return 0;
-		}else if(numbers.contains(",")) {
-			String[] nos = numbers.split(",");
+		}else if(numbers.contains(",")||numbers.contains("\\n")) {
+			String[] nos = numbers.split(",|\\\\n");			
 			int s = 0;
 			for(int i=0;i<nos.length;i++) {
 				s+=Integer.parseInt(nos[i]);
